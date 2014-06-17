1f1be98306de9a230f59fae5b987aff83594824d
Bernd Vogt <bernd.vogt@sourcepit.org>
2014-06-17 11:51:33 +0200

    Inlined RepositoryUtils.toArtifact(...)

--- common-maven-core/src/main/java/org/sourcepit/common/maven/aether/ArtifactFactory.java
+++ common-maven-core/src/main/java/org/sourcepit/common/maven/aether/ArtifactFactory.java
@@ -9,15 +9,18 @@ package org.sourcepit.common.maven.aether;
 import static com.google.common.base.Strings.isNullOrEmpty;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.apache.maven.RepositoryUtils;
 import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.artifact.handler.ArtifactHandler;
 import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.ArtifactProperties;
+import org.eclipse.aether.artifact.ArtifactType;
+import org.eclipse.aether.artifact.DefaultArtifactType;
 import org.sourcepit.common.maven.model.ArtifactKey;
 
 @Named
@@ -39,20 +42,20 @@ public class ArtifactFactory
    public Artifact createArtifact(final Artifact artifact, String classifier, String type, String localPath)
    {
       classifier = isNullOrEmpty(classifier) ? null : classifier;
-      
+
       final String scope = localPath == null ? null : org.apache.maven.artifact.Artifact.SCOPE_SYSTEM;
 
       final ArtifactHandler handler = artifactHandlers.get(type);
 
       final DefaultArtifact legacyArtifact = new DefaultArtifact(artifact.getGroupId(), artifact.getArtifactId(),
          artifact.getVersion(), scope, type, classifier, handler);
-      
+
       if (localPath != null)
       {
          legacyArtifact.setFile(new File(localPath));
       }
 
-      return RepositoryUtils.toArtifact(legacyArtifact);
+      return toArtifact(legacyArtifact);
    }
 
    public Artifact createArtifact(ArtifactKey artifactKey)
@@ -74,6 +77,56 @@ public class ArtifactFactory
          legacyArtifact.setFile(new File(localPath));
       }
 
-      return RepositoryUtils.toArtifact(legacyArtifact);
+      return toArtifact(legacyArtifact);
+   }
+
+   private static Artifact toArtifact(org.apache.maven.artifact.Artifact artifact)
+   {
+      final String groupId = artifact.getGroupId();
+      final String artifactId = artifact.getArtifactId();
+      final String version = getVersion(artifact);
+      final String classifier = artifact.getClassifier();
+      final ArtifactHandler artifactHandler = artifact.getArtifactHandler();
+      final String extension = artifactHandler.getExtension();
+      final Map<String, String> properties = getProperties(artifact);
+      final String type = artifact.getType();
+      final ArtifactType artifactType = newArtifactType(type, artifactHandler);
+      final File file = artifact.getFile();
+      return newArtifact(groupId, artifactId, version, classifier, extension, properties, artifactType, file);
+   }
+
+   private static Artifact newArtifact(final String groupId, final String artifactId, final String version,
+      final String classifier, final String extension, final Map<String, String> properties,
+      final ArtifactType artifactType, final File file)
+   {
+      return new org.eclipse.aether.artifact.DefaultArtifact(groupId, artifactId, classifier, extension, version,
+         properties, artifactType).setFile(file);
+   }
+
+   private static Map<String, String> getProperties(org.apache.maven.artifact.Artifact artifact)
+   {
+      final String scope = artifact.getScope();
+      if (org.apache.maven.artifact.Artifact.SCOPE_SYSTEM.equals(scope))
+      {
+         String localPath = (artifact.getFile() != null) ? artifact.getFile().getPath() : "";
+         return Collections.singletonMap(ArtifactProperties.LOCAL_PATH, localPath);
+      }
+      return null;
+   }
+
+   private static String getVersion(org.apache.maven.artifact.Artifact artifact)
+   {
+      String version = artifact.getVersion();
+      if (version == null && artifact.getVersionRange() != null)
+      {
+         version = artifact.getVersionRange().toString();
+      }
+      return version;
+   }
+
+   private static ArtifactType newArtifactType(String id, ArtifactHandler handler)
+   {
+      return new DefaultArtifactType(id, handler.getExtension(), handler.getClassifier(), handler.getLanguage(),
+         handler.isAddedToClasspath(), handler.isIncludesDependencies());
    }
 }
