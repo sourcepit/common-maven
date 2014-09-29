b0a07439361700025ddb1240e0ac0935cc97ae7c
Bernd Vogt <bernd.vogt@sourcepit.org>
2014-09-29 11:22:47 +0200

    #1: Apache License Version 2.0

--- org.sourcepit.common.maven.model/src/main/java/org/sourcepit/common/maven/model/util/MavenModelUtils.java
+++ org.sourcepit.common.maven.model/src/main/java/org/sourcepit/common/maven/model/util/MavenModelUtils.java
@@ -1,7 +1,17 @@
-/**
- * Copyright (c) 2011 Sourcepit.org contributors and others. All rights reserved. This program and the accompanying
- * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+/*
+ * Copyright 2014 Bernd Vogt and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.sourcepit.common.maven.model.util;
@@ -32,7 +42,7 @@ import org.sourcepit.common.maven.model.VersionConflictKey;
 
 public final class MavenModelUtils
 {
-   
+
 
    @NotNull
    public static MavenDependency toMavenDependecy(@NotNull Dependency dependency)
@@ -256,8 +266,8 @@ public final class MavenModelUtils
       }
       return null;
    }
-   
-   private static final Pattern VERSION_FILE_PATTERN = Pattern.compile( "^(.*)-([0-9]{8}.[0-9]{6})-([0-9]+)$" );
+
+   private static final Pattern VERSION_FILE_PATTERN = Pattern.compile("^(.*)-([0-9]{8}.[0-9]{6})-([0-9]+)$");
 
    public static String normalizeSnapshotVersion(@NotNull String version)
    {
